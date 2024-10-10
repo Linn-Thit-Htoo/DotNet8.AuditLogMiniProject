709a8bace2e03d3f80ac5c79807e5e60cbdff798
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-11 00:22:46 +0630

    [ Added ] AddBlogAsync

---
+++ DotNet8.AuditLogMiniProject.Extensions/Extension.cs
@@ -0,0 +1,23 @@
+﻿using DotNet8.AuditLogMiniProject.Domain.Features.Blog;
+using DotNet8.AuditLogMiniProject.DTOs.Features.Blog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.AuditLogMiniProject.Extensions
+{
+    public static class Extension
+    {
+        public static Tbl_Blog ToEntity(this BlogRequestDTO blogRequest)
+        {
+            return new Tbl_Blog
+            {
+                BlogTitle = blogRequest.BlogTitle,
+                BlogAuthor = blogRequest.BlogAuthor,
+                BlogContent = blogRequest.BlogContent
+            };
+        }
+    }
+}
--- DotNet8.AuditLogMiniProject.Infrastructure/Features/Blog/BlogService.cs
+++ DotNet8.AuditLogMiniProject.Infrastructure/Features/Blog/BlogService.cs
@@ -1,5 +1,6 @@
 ﻿using DotNet8.AuditLogMiniProject.Domain.Features.Blog;
 using DotNet8.AuditLogMiniProject.DTOs.Features.Blog;
+using DotNet8.AuditLogMiniProject.Extensions;
 using DotNet8.AuditLogMiniProject.Utils;
 using System;
 using System.Collections.Generic;
@@ -12,9 +13,29 @@ namespace DotNet8.AuditLogMiniProject.Infrastructure.Features.Blog
     public class BlogService : IBlogService
     {
         private readonly AppDbContext _context;
-        public Task<Result<BlogDTO>> AddBlogAsync(BlogRequestDTO blogRequest, CancellationToken cs)
+
+        public BlogService(AppDbContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+        }
+
+        public async Task<Result<BlogDTO>> AddBlogAsync(BlogRequestDTO blogRequest, CancellationToken cs)
+        {
+            Result<BlogDTO> result;
+            try
+            {
+                await _context.Tbl_Blog.AddAsync(blogRequest.ToEntity(), cs);
+                await _context.SaveChangesAsync(cs);
+
+                result = Result<BlogDTO>.Success();
+            }
+            catch (Exception ex)
+            {
+                result = Result<BlogDTO>.Fail(ex);
+            }
+
+        result:
+            return result;
         }
     }
 }
