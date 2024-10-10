56b802f00024c881c42600f505187a8639449437
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-11 01:11:24 +0630

    [ Added ] UpdateBlogAsync

--- DotNet8.AuditLogMiniProject.Domain/Features/Blog/IBlogService.cs
+++ DotNet8.AuditLogMiniProject.Domain/Features/Blog/IBlogService.cs
@@ -11,5 +11,6 @@ namespace DotNet8.AuditLogMiniProject.Domain.Features.Blog
     public interface IBlogService
     {
         Task<Result<BlogDTO>> AddBlogAsync(BlogRequestDTO blogRequest, CancellationToken cs);
+        Task<Result<BlogDTO>> UpdateBlogAsync(BlogRequestDTO blogRequest, int id, CancellationToken cs);
     }
 }
--- DotNet8.AuditLogMiniProject.Infrastructure/Features/Blog/BlogService.cs
+++ DotNet8.AuditLogMiniProject.Infrastructure/Features/Blog/BlogService.cs
@@ -37,5 +37,35 @@ namespace DotNet8.AuditLogMiniProject.Infrastructure.Features.Blog
         result:
             return result;
         }
+
+        public async Task<Result<BlogDTO>> UpdateBlogAsync(BlogRequestDTO blogRequest, int id, CancellationToken cs)
+        {
+            Result<BlogDTO> result;
+            try
+            {
+                var item = await _context.Tbl_Blog.FindAsync(blogRequest.ToEntity(), cs);
+                if (item is null)
+                {
+                    result = Result<BlogDTO>.NotFound();
+                    goto result;
+                }
+
+                item.BlogTitle = blogRequest.BlogTitle;
+                item.BlogAuthor = blogRequest.BlogAuthor;
+                item.BlogContent = blogRequest.BlogContent;
+
+                _context.Tbl_Blog.Update(item);
+                await _context.SaveChangesAsync(cs);
+
+                result = Result<BlogDTO>.Success();
+            }
+            catch (Exception ex)
+            {
+                result = Result<BlogDTO>.Fail(ex);
+            }
+
+        result:
+            return result;
+        }
     }
 }
