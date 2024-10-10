58df60cbce31f2ee5ce5d7bae4a5e5f53c91c705
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-11 00:18:45 +0630

    -

---
+++ DotNet8.AuditLogMiniProject.Domain/Features/Blog/IBlogService.cs
@@ -0,0 +1,15 @@
+﻿using DotNet8.AuditLogMiniProject.DTOs.Features.Blog;
+using DotNet8.AuditLogMiniProject.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.AuditLogMiniProject.Domain.Features.Blog
+{
+    public interface IBlogService
+    {
+        Task<Result<BlogDTO>> AddBlogAsync(BlogRequestDTO blogRequest, CancellationToken cs);
+    }
+}
---
+++ DotNet8.AuditLogMiniProject.Infrastructure/Features/Blog/BlogService.cs
@@ -0,0 +1,20 @@
+﻿using DotNet8.AuditLogMiniProject.Domain.Features.Blog;
+using DotNet8.AuditLogMiniProject.DTOs.Features.Blog;
+using DotNet8.AuditLogMiniProject.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.AuditLogMiniProject.Infrastructure.Features.Blog
+{
+    public class BlogService : IBlogService
+    {
+        private readonly AppDbContext _context;
+        public Task<Result<BlogDTO>> AddBlogAsync(BlogRequestDTO blogRequest, CancellationToken cs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
