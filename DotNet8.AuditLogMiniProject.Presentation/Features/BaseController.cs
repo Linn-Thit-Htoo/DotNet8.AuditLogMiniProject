308a7eab0f90486572af466fedb972b2a80ee2ee
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-11 09:25:18 +0630

    -

--- DotNet8.AuditLogMiniProject.Presentation/Features/BaseController.cs
+++ DotNet8.AuditLogMiniProject.Presentation/Features/BaseController.cs
@@ -1,15 +1,14 @@
 ﻿using DotNet8.AuditLogMiniProject.Shared;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotNet8.AuditLogMiniProject.Presentation.Features
+namespace DotNet8.AuditLogMiniProject.Presentation.Features;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BaseController : ControllerBase
+    protected IActionResult Content(object obj)
     {
-        protected IActionResult Content(object obj)
-        {
-            return Content(obj.ToJson(), "application/json");
-        }
+        return Content(obj.ToJson(), "application/json");
     }
 }
