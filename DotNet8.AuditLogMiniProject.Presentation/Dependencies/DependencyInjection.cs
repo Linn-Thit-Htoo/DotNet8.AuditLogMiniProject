74e8a08bb3f043f7d7152686e16885a8a61dd924
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-11 09:24:57 +0630

    -

--- DotNet8.AuditLogMiniProject.Presentation/Dependencies/DependencyInjection.cs
+++ DotNet8.AuditLogMiniProject.Presentation/Dependencies/DependencyInjection.cs
@@ -1,26 +1,25 @@
-﻿namespace DotNet8.AuditLogMiniProject.Presentation.Dependencies
+﻿namespace DotNet8.AuditLogMiniProject.Presentation.Dependencies;
+
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            return services.AddDbContextService(builder).AddDataAccessServices();
-        }
+        return services.AddDbContextService(builder).AddDataAccessServices();
+    }
 
-        private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+    private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<AppDbContext>(opt =>
         {
-            builder.Services.AddDbContext<AppDbContext>(opt =>
-            {
-                opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-                opt.AddInterceptors(new AuditInterceptor());
-            }, ServiceLifetime.Transient, ServiceLifetime.Transient);
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+            opt.AddInterceptors(new AuditInterceptor());
+        }, ServiceLifetime.Transient, ServiceLifetime.Transient);
 
-            return services;
-        }
+        return services;
+    }
 
-        private static IServiceCollection AddDataAccessServices(this IServiceCollection services)
-        {
-            return services.AddScoped<IBlogService, BlogService>();
-        }
+    private static IServiceCollection AddDataAccessServices(this IServiceCollection services)
+    {
+        return services.AddScoped<IBlogService, BlogService>();
     }
 }
