ff033984ad04b0802b38be6b0f18c15889d1d7db
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-11 09:26:07 +0630

    -

--- DotNet8.AuditLogMiniProject.Presentation/Features/Blog/BlogController.cs
+++ DotNet8.AuditLogMiniProject.Presentation/Features/Blog/BlogController.cs
@@ -1,28 +1,27 @@
-﻿namespace DotNet8.AuditLogMiniProject.Presentation.Features.Blog
+﻿namespace DotNet8.AuditLogMiniProject.Presentation.Features.Blog;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : BaseController
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogController : BaseController
+    private readonly IBlogService _blogService;
+
+    public BlogController(IBlogService blogService)
     {
-        private readonly IBlogService _blogService;
+        _blogService = blogService;
+    }
 
-        public BlogController(IBlogService blogService)
-        {
-            _blogService = blogService;
-        }
+    [HttpPost]
+    public async Task<IActionResult> CreateBlog([FromBody] BlogRequestDTO blogRequest, CancellationToken cs)
+    {
+        var result = await _blogService.AddBlogAsync(blogRequest, cs);
+        return Content(result);
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> CreateBlog([FromBody] BlogRequestDTO blogRequest, CancellationToken cs)
-        {
-            var result = await _blogService.AddBlogAsync(blogRequest, cs);
-            return Content(result);
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateBlog([FromBody] BlogRequestDTO blogRequest, int id, CancellationToken cs)
-        {
-            var result = await _blogService.UpdateBlogAsync(blogRequest, id, cs);
-            return Content(result);
-        }
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateBlog([FromBody] BlogRequestDTO blogRequest, int id, CancellationToken cs)
+    {
+        var result = await _blogService.UpdateBlogAsync(blogRequest, id, cs);
+        return Content(result);
     }
 }
