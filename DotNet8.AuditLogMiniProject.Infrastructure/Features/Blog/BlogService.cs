9fd2b6844c3ffa7e017ce274c61a7a7fb737c651
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-11 09:19:57 +0630

    -

--- DotNet8.AuditLogMiniProject.Infrastructure/Features/Blog/BlogService.cs
+++ DotNet8.AuditLogMiniProject.Infrastructure/Features/Blog/BlogService.cs
@@ -3,64 +3,63 @@ using DotNet8.AuditLogMiniProject.DTOs.Features.Blog;
 using DotNet8.AuditLogMiniProject.Extensions;
 using DotNet8.AuditLogMiniProject.Utils;
 
-namespace DotNet8.AuditLogMiniProject.Infrastructure.Features.Blog
+namespace DotNet8.AuditLogMiniProject.Infrastructure.Features.Blog;
+
+public class BlogService : IBlogService
 {
-    public class BlogService : IBlogService
+    private readonly AppDbContext _context;
+
+    public BlogService(AppDbContext context)
     {
-        private readonly AppDbContext _context;
+        _context = context;
+    }
 
-        public BlogService(AppDbContext context)
+    public async Task<Result<BlogDTO>> AddBlogAsync(BlogRequestDTO blogRequest, CancellationToken cs)
+    {
+        Result<BlogDTO> result;
+        try
         {
-            _context = context;
+            await _context.Tbl_Blog.AddAsync(blogRequest.ToEntity(), cs);
+            await _context.SaveChangesAsync(cs);
+
+            result = Result<BlogDTO>.Success();
+        }
+        catch (Exception ex)
+        {
+            result = Result<BlogDTO>.Fail(ex);
         }
 
-        public async Task<Result<BlogDTO>> AddBlogAsync(BlogRequestDTO blogRequest, CancellationToken cs)
+    result:
+        return result;
+    }
+
+    public async Task<Result<BlogDTO>> UpdateBlogAsync(BlogRequestDTO blogRequest, int id, CancellationToken cs)
+    {
+        Result<BlogDTO> result;
+        try
         {
-            Result<BlogDTO> result;
-            try
+            var item = await _context.Tbl_Blog.FindAsync([id, cs], cancellationToken: cs);
+            if (item is null)
             {
-                await _context.Tbl_Blog.AddAsync(blogRequest.ToEntity(), cs);
-                await _context.SaveChangesAsync(cs);
-
-                result = Result<BlogDTO>.Success();
-            }
-            catch (Exception ex)
-            {
-                result = Result<BlogDTO>.Fail(ex);
+                result = Result<BlogDTO>.NotFound();
+                goto result;
             }
 
-        result:
-            return result;
+            item.BlogTitle = blogRequest.BlogTitle;
+            item.BlogAuthor = blogRequest.BlogAuthor;
+            item.BlogContent = blogRequest.BlogContent;
+
+            _context.Tbl_Blog.Update(item);
+            await _context.SaveChangesAsync(cs);
+
+            result = Result<BlogDTO>.Success();
+        }
+        catch (Exception ex)
+        {
+            result = Result<BlogDTO>.Fail(ex);
         }
 
-        public async Task<Result<BlogDTO>> UpdateBlogAsync(BlogRequestDTO blogRequest, int id, CancellationToken cs)
-        {
-            Result<BlogDTO> result;
-            try
-            {
-                var item = await _context.Tbl_Blog.FindAsync([id, cs], cancellationToken: cs);
-                if (item is null)
-                {
-                    result = Result<BlogDTO>.NotFound();
-                    goto result;
-                }
-
-                item.BlogTitle = blogRequest.BlogTitle;
-                item.BlogAuthor = blogRequest.BlogAuthor;
-                item.BlogContent = blogRequest.BlogContent;
-
-                _context.Tbl_Blog.Update(item);
-                await _context.SaveChangesAsync(cs);
-
-                result = Result<BlogDTO>.Success();
-            }
-            catch (Exception ex)
-            {
-                result = Result<BlogDTO>.Fail(ex);
-            }
-
-        result:
-            return result;
-        }
+    result:
+        return result;
     }
 }
