357288c17de0106d6a93dd88a869cb42bc7d1263
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-11 09:09:13 +0630

    [ Modified ] Audit Table

---
+++ DotNet8.AuditLogMiniProject.Infrastructure/Migrations/20241011023836_ChangeInAudit.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotNet8.AuditLogMiniProject.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeInAudit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Tbl_Audit",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Tbl_Audit");
+        }
+    }
+}
