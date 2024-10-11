31477270c78a465227192669f6add3aa10e6fc85
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-11 09:19:34 +0630

    -

--- DotNet8.AuditLogMiniProject.Domain/Features/Blog/Tbl_Blog.cs
+++ DotNet8.AuditLogMiniProject.Domain/Features/Blog/Tbl_Blog.cs
@@ -1,11 +1,10 @@
-﻿namespace DotNet8.AuditLogMiniProject.Domain.Features.Blog
+﻿namespace DotNet8.AuditLogMiniProject.Domain.Features.Blog;
+
+public class Tbl_Blog
 {
-    public class Tbl_Blog
-    {
-        [Key]
-        public int BlogId { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
-    }
+    [Key]
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
 }
