218cc05eca195f317b8a11f4ee8828385ac9da63
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-11 09:21:29 +0630

    -

--- DotNet8.AuditLogMiniProject.Infrastructure/Interceptors/AuditInterceptor.cs
+++ DotNet8.AuditLogMiniProject.Infrastructure/Interceptors/AuditInterceptor.cs
@@ -1,50 +1,49 @@
-﻿namespace DotNet8.AuditLogMiniProject.Infrastructure.Interceptors
+﻿namespace DotNet8.AuditLogMiniProject.Infrastructure.Interceptors;
+
+public class AuditInterceptor : SaveChangesInterceptor
 {
-    public class AuditInterceptor : SaveChangesInterceptor
+    public override async ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
     {
-        public override async ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+        var context = eventData.Context!;
+        var addedEntries = context.ChangeTracker.Entries()
+                                  .Where(x => x.State == EntityState.Added)
+                                  .ToList();
+        var modifiedEntries = context.ChangeTracker.Entries()
+                                     .Where(x => x.State == EntityState.Modified)
+                                     .ToList();
+
+        if (addedEntries.Count != 0)
         {
-            var context = eventData.Context!;
-            var addedEntries = context.ChangeTracker.Entries()
-                                      .Where(x => x.State == EntityState.Added)
-                                      .ToList();
-            var modifiedEntries = context.ChangeTracker.Entries()
-                                         .Where(x => x.State == EntityState.Modified)
-                                         .ToList();
-
-            if (addedEntries.Count != 0)
+            foreach (var entry in addedEntries)
             {
-                foreach (var entry in addedEntries)
+                var audit = new Tbl_Audit
                 {
-                    var audit = new Tbl_Audit
-                    {
-                        CreatedDate = DateTime.Now,
-                        EntityName = entry.Entity.GetType().Name,
-                        Operation = "Created",
-                    };
+                    CreatedDate = DateTime.Now,
+                    EntityName = entry.Entity.GetType().Name,
+                    Operation = "Created",
+                };
 
-                    await context.Set<Tbl_Audit>().AddAsync(audit, cancellationToken);
-                }
+                await context.Set<Tbl_Audit>().AddAsync(audit, cancellationToken);
             }
-
-            if (modifiedEntries.Count != 0)
-            {
-                foreach (var entry in modifiedEntries)
-                {
-                    var entityId = entry.Properties.First(p => p.Metadata.IsPrimaryKey()).CurrentValue;
-                    var audit = new Tbl_Audit
-                    {
-                        UpdatedDate = DateTime.Now,
-                        EntityName = entry.Entity.GetType().Name,
-                        Operation = "Updated",
-                        Id = Convert.ToInt32(entityId)
-                    };
-
-                    await context.Set<Tbl_Audit>().AddAsync(audit, cancellationToken);
-                }
-            }
-
-            return await base.SavingChangesAsync(eventData, result, cancellationToken);
         }
+
+        if (modifiedEntries.Count != 0)
+        {
+            foreach (var entry in modifiedEntries)
+            {
+                var entityId = entry.Properties.First(p => p.Metadata.IsPrimaryKey()).CurrentValue;
+                var audit = new Tbl_Audit
+                {
+                    UpdatedDate = DateTime.Now,
+                    EntityName = entry.Entity.GetType().Name,
+                    Operation = "Updated",
+                    Id = Convert.ToInt32(entityId)
+                };
+
+                await context.Set<Tbl_Audit>().AddAsync(audit, cancellationToken);
+            }
+        }
+
+        return await base.SavingChangesAsync(eventData, result, cancellationToken);
     }
 }
