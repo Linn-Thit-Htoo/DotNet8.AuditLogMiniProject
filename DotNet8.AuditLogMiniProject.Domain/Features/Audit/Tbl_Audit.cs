5d9d8489189da75d665c4c966c4293cd2622287f
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-11 09:18:27 +0630

    -

--- DotNet8.AuditLogMiniProject.Domain/Features/Audit/Tbl_Audit.cs
+++ DotNet8.AuditLogMiniProject.Domain/Features/Audit/Tbl_Audit.cs
@@ -1,15 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace DotNet8.AuditLogMiniProject.Domain.Features.Audit
+namespace DotNet8.AuditLogMiniProject.Domain.Features.Audit;
+
+public class Tbl_Audit
 {
-    public class Tbl_Audit
-    {
-        [Key]
-        public int AuditId { get; set; }
-        public int? Id { get; set; }
-        public string EntityName { get; set; }
-        public string Operation { get; set; }
-        public DateTime? CreatedDate { get; set; }
-        public DateTime? UpdatedDate { get; set; }
-    }
+    [Key]
+    public int AuditId { get; set; }
+    public int? Id { get; set; }
+    public string EntityName { get; set; }
+    public string Operation { get; set; }
+    public DateTime? CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
 }
