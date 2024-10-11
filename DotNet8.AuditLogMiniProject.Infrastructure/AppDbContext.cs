a989d996a16fcabc1a1b5301b7bfab193e52aa98
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-11 09:21:57 +0630

    -

--- DotNet8.AuditLogMiniProject.Infrastructure/AppDbContext.cs
+++ DotNet8.AuditLogMiniProject.Infrastructure/AppDbContext.cs
@@ -2,9 +2,8 @@
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions options) : base(options)
-    {
-    }
+    public AppDbContext(DbContextOptions options)
+        : base(options) { }
 
     public DbSet<Tbl_Blog> Tbl_Blog { get; set; }
     public DbSet<Tbl_Audit> Tbl_Audit { get; set; }
